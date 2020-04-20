b6c521ea7579d17c30557f82c7d4bb0bde3ae621
Mayur290 <mayurbansal98@gmail.com>
2020-04-20 13:20:03 +0530

    created simple person component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+      </div>
+    );
+  }
 }
 
 export default App;
