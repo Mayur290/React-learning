195df1adc4c12b3c097d8f372f2a9d8c26b5057b
Mayur290 <mayurbansal98@gmail.com>
2020-04-20 13:28:10 +0530

    class based component using \'state & props\'

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 18 },
+      { name: "manu", age: 24 },
+      { name: "stephinie", age: 28 },
+    ],
+    otherState: "some other state",
+  };
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        ></Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          Hi I love that girl.
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        ></Person>
       </div>
     );
   }
